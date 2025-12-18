e7301bd29ded54793feca07c1cc3955ebc260595
andrewflynaut <129895056+andrewflynaut@users.noreply.github.com>
2025-12-18 12:59:50 +0530

    c

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -16,19 +16,15 @@ function Signup() {
     const user = { name, email, password };
     localStorage.setItem("user", JSON.stringify(user));
 
-    alert("Signup successful!");
+    alert("Signup successful");
   };
 
   return (
     <form onSubmit={handleSignup}>
       <h2>Signup</h2>
-
-      <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-
-      <input
-        type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+         <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
       <button type="submit">Signup</button>
     </form>
